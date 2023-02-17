77cfe7c59913b2a3e4b9038658db659bbcc5d305
Gil Dobrovinsky <gdobrovinsky@gmail.com>
2023-02-17 18:47:00 -0500

    Created user Type Defs

---
+++ schema.js
@@ -0,0 +1,35 @@
+const { gql } = require('apollo-server-express');
+
+exports.typeDefs = gql `
+    type User {
+        id: ID
+        username: String
+        email: String
+        password: String
+    }
+
+    type Query {
+        users: [User]
+        user(id: ID!): User
+    }
+
+    type Mutation {
+        createUser(
+            username: String
+            email: String
+            password: String
+        ): User
+
+
+        updateEmployee(id: String!
+            firstname: String!
+            lastname: String!
+            email: String!
+            gender: String!
+            city: String!
+            designation: String!
+            salary: Float!): Employee
+        
+        deleteEmployee(id: String!): Employee
+    }
+`
